6e317bfb431e1c46de5ae6b288c73ccd3f8ae7ff
Pavel Vorontsov <pvorontsovd@gmail.com>
2018-07-23 23:15:48 +0300

    6.2 done

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -14,3 +14,9 @@
 mac = ['aabb:cc80:7000', 'aabb:dd80:7340', 'aabb:ee80:7000', 'aabb:ff80:7000']
 
 mac_cisco = []
+
+for m in mac:
+    mCisco = m.replace(':', '.')
+    mac_cisco.append(mCisco)
+
+print(mac_cisco)
