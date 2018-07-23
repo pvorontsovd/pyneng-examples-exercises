92e411ac3e3187e87ad244b8613dcd4806a50a1d
Pavel Vorontsov <pvorontsovd@gmail.com>
2018-07-23 23:15:36 +0300

    6.1b done

--- exercises/06_control_structures/task_6_1b.py
+++ exercises/06_control_structures/task_6_1b.py
@@ -9,3 +9,37 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+validIP = False
+while not validIP:
+    try:
+        ipAdress = input('введите ip адрес: ')
+
+        ipAdressList = ipAdress.split('.')
+
+        if len(ipAdressList) == 4:
+            validIP = True
+            try:
+                for octet in ipAdressList:
+                    if not 0 <= int(octet) <= 255:
+                        validIP = False
+                        break
+            except ValueError:
+                validIP = False
+        else:
+            validIP = False
+    except KeyboardInterrupt:
+        print('\nbye!')
+        break
+else:
+    firstOctet = int(ipAdressList[0])
+    if 1 <= firstOctet <= 223:
+        print('unicast')
+    elif 224 <= firstOctet <= 239:
+        print('multicast')
+    elif ipAdress == '255.255.255.255':
+        print('local broadcast')
+    elif ipAdress == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
