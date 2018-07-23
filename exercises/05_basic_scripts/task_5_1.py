4ce4a4c8bb0638d3aecf251276c53d497fe0cb80
Pavel Vorontsov <pvorontsovd@gmail.com>
2018-07-23 23:06:33 +0300

    5.1 done

--- exercises/05_basic_scripts/task_5_1.py
+++ exercises/05_basic_scripts/task_5_1.py
@@ -19,3 +19,22 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+inputData = input('введите IP-сеть в формате network/mask: ')
+
+network = list(int(i) for i in (inputData.split('/')[0]).split('.'))
+mask = int(inputData.split('/')[1])
+
+bitMask = ('1' * mask + '0' * (32 - mask))
+
+print(f'''\
+network:
+{network[0]:<8}  {network[1]:<8}  {network[2]:<8}  {network[3]:<8}
+{network[0]:08b}  {network[1]:08b}  {network[2]:08b}  {network[3]:08b}
+
+mask:
+/{mask}
+{int(bitMask[0:8], 2):<8}  {int(bitMask[8:16], 2):<8}  \
+{int(bitMask[16:24], 2):<8}  {int(bitMask[24:32], 2):<8}
+{bitMask[0:8]}  {bitMask[8:16]}  {bitMask[16:24]}  {bitMask[24:32]}\
+''')
