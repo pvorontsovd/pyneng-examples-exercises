a7a661db145b472ce230044167821ccaf1277ea6
Pavel Vorontsov <pvorontsovd@gmail.com>
2018-07-30 00:16:39 +0300

    9.4a done

--- exercises/09_functions/task_9_4a.py
+++ exercises/09_functions/task_9_4a.py
@@ -49,3 +49,55 @@ def check_ignore(command, ignore):
 
     '''
     return any(word in command for word in ignore)
+
+
+def get_subcommands(commands, key, level):
+    '''
+    Функция возвращает список всех "сабкоманд" от конкретной команды
+    commands - список команд
+    key - команда, от которой нужно найти сабкоманды
+    level - уровень вложенности (один пробел или два пробела)
+    '''
+    subcommands = []
+    index = commands.index(key)
+    while True:
+        try:
+            elem = commands[index + 1]
+            if elem.startswith(level):
+                subcommands.append(elem)
+                index += 1
+            else:
+                return subcommands
+        except IndexError:
+            return subcommands
+
+
+def config_to_dict(config_file):
+    commands = []
+    with open(config_file, 'r') as f:
+        for line in f:
+            if line.startswith('!') or check_ignore(line, ignore):
+                continue
+            commands.append(line.rstrip())
+
+    dict_config = {key: None for key in commands if not key.startswith(' ')}
+
+    for key in dict_config:
+        subcommands = get_subcommands(commands, key, ' ')
+        if not subcommands:
+            dict_config[key] = []
+        elif subcommands[-1].startswith('  '):
+            sub_dict = {key: [] for key in subcommands if not key.startswith('  ')}
+            for sub_key in sub_dict:
+                sub_subcom = get_subcommands(subcommands, sub_key, '  ')
+                sub_dict[sub_key] = sub_subcom
+            dict_config[key] = sub_dict
+        else: # if subcommands[-1].startswith(' '):
+            dict_config[key] = []
+            for command in subcommands:
+                dict_config[key].append(command)
+
+    return dict_config
+
+
+print(config_to_dict('config_r1.txt'))
