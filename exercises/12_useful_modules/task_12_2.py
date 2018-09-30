daf190d6512195186e6d0984090e979d64c2c436
Pavel Vorontsov <pvorontsovd@gmail.com>
2018-09-30 22:40:54 +0300

    12.2 done

--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -29,3 +29,35 @@ IP-адреса могут быть в формате:
 
 Для выполнения задачи можно воспользоваться функцией check_ip_addresses из задания 12.1
 '''
+
+from sys import argv
+from subprocess import run, PIPE
+
+data = argv[1:]
+
+
+def parse_arguments(data):
+    try:
+        net, host = data[0].split('-')
+        first = net[-1]
+        net = net[:-1]
+        ip_list = [net+str(x) for x in range(int(first), int(host)+1)]
+        return ip_list
+    except ValueError:
+        return data
+
+
+def check_ip_addresses(ip_list):
+    ok = []
+    nok = []
+    for ip in ip_list:
+        res = run(f'ping {ip} -c 3', shell=True, stdout=PIPE, encoding='utf-8')
+        for line in res.stdout.split('\n'):
+            if line.startswith('3 packets'):
+                loss = line.split()[-3]
+                ok.append(ip) if loss == '0.0%' else nok.append(ip)
+    return ok, nok
+
+
+if __name__ == '__main__':
+    print(check_ip_addresses(parse_arguments(data)))
